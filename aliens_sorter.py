442634d8e986bf5a8c1605419172fd58e6563e41
Talih Bayram <talihbayram@users.noreply.github.com>
2018-04-14 03:12:30 +0300

    Scripts doesn't write files cause of some issues,fixed
    Planet.csv output file truncation, hard coded planets name removed strings in for loops,  no need to add "+"\n" removed. Connection not closed, added at the end of script.

--- aliens_sorter.py
+++ aliens_sorter.py
@@ -1,7 +1,9 @@
-f = open('aliens.csv')
+input_file="aliens.csv"
+f = open(input_file)
 aliens = f.readlines()
 aliensList = []
 planetsList = list()
+UniquePList=[]
 header = True
 for line in aliens:
     if not header:
@@ -12,7 +14,12 @@ for line in aliens:
     else:
         header = False
 
-output = open('planets.csv',mode="w")
+for upl in planetsList:
+    if upl not in UniquePList:
+        UniquePList.append(upl)
+
+output_file="planets.csv"
+output=open(output_file,mode="w+")
 output.write("species,planet\n")
 
 planet_zerg = []
@@ -21,35 +28,36 @@ planet_mars = []
 
 for alien in aliensList:
     planet = planetsList[aliensList.index(alien)]
-    if planet==" zerg-1":
+    if planet==UniquePList[0]:
         planet_zerg.append(alien)
-    if planet==" ho":
+    if planet==UniquePList[1]:
         planet_ho.append(alien)
-    if planet==" mars":
+    if planet==UniquePList[2]:
         planet_mars.append(alien)
-
 
 class Zergalizer:
     def do_zergalization(self, species):
         transformed = list()
         for s in species:
             transformed.append(s.upper())
-        output.write(" ".join(transformed)+",zerg"+"\n")
+        output.write(" ".join(transformed)+",zerg\n")
+##you can do like parameter output.write(" ".join(transformed)+ "," + UniquePList[0]+ "\n")
 
 class Hoizer:
     def do_hoizification(self, species):
         transformed = list()
         for s in species:
             transformed.append(s+"HO!")
-        output.write((" | ").join(transformed)+",ho"+"\n")
+        output.write((" | ").join(transformed)+",ho\n")
 
 class Marsianizer:
     def do_marsianization(self, species):
         transformed = list()
         for s in species:
             transformed.append(" ".join(s.split("."))+"m")
-        output.write((" ~~ ").join(transformed)+",mars"+"\n")
+        output.write((" ~~ ").join(transformed)+",mars\n")
 
 Zergalizer().do_zergalization(planet_zerg)
 Hoizer().do_hoizification(planet_ho)
 Marsianizer().do_marsianization(planet_mars)
+output.close()
